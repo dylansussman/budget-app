87e70c6d7a7d945317b561a973141143f5b4e575
dylansussman <dylansussman17@gmail.com>
2026-03-17 20:49:52 -0500

    Initial functionality for capital one transactions

--- main.py
+++ main.py
@@ -111,9 +111,15 @@ async def upload_csv_files(files: list[UploadFile] = File(...)):
                 file_obj = io.BytesIO(contents)
 
                 filename = file.filename.lower()
-                source = "chase" if "chase" in filename else "generic"
-                account = filename[filename.find(source)+1:filename.find("_")] if source == "chase" else "unknown"
-                
+                account = "unknown"
+                if "chase" in filename:
+                    source = "chase"
+                    account = filename[filename.find(source)+1:filename.find("_")]
+                elif "capitalone" in filename:
+                    source = "capitalone"
+                else:
+                    source = "generic"
+                                    
                 # Parse CSV using parser.py (auto-detect format)
                 transactions = parse_csv(file_obj, source=source, account=account)
                 
